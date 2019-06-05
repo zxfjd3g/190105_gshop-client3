8c062e352a8d3d518415aeacaf6ceecf949d4992
zxfjd3g <258147149@qq.com>
2019-06-05 15:52:06 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Header from './components/Header/Header.vue'
+import Star from './components/Star/Star.vue'
 import store from './vuex/store'
 
 Vue.config.productionTip = false
 
 // 注册全局组件
 Vue.component('Header', Header)
+Vue.component('Star', Star)
 
 
 /* eslint-disable no-new */
--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,16 +4,17 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on: loginWay}" @click="loginWay=true">短信登录</a>
+          <a href="javascript:;" :class="{on: !loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on: loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!isRightPhone" class="get_verification" 
+              :class="{right_phone_number: isRightPhone}" @click.prevent="sendCode">获取验证码</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,7 +24,7 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on: !loginWay}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
@@ -54,7 +55,25 @@
 
 <script type="text/ecmascript-6">
   export default {
+    data () {
+      return {
+        loginWay: true, // true: 短信登陆, false: 密码登陆
+        phone: '', // 手机号
+      }
+    },
 
+    computed: {
+      // 判断phone是否是一个正确的手机号
+      isRightPhone () {
+        return /^1\d{10}$/.test(this.phone)
+      }
+    },
+
+    methods: {
+      sendCode () {
+        alert('----')
+      }
+    }
   }
 </script>
 
@@ -119,6 +138,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone_number
+                  color black
             .login_verification
               position relative
               margin-top 16px
